dc79cdcc970efcd87dbbc2bc83bca91ce4452904
Angel Rodriguez <angel01rdz@outlook.com>
2023-02-11 15:58:29 -0600

    Fix function

--- pages/api/amenities/[id].js
+++ pages/api/amenities/[id].js
@@ -1,19 +1,18 @@
-const superagent = require("superagent");
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const number = Number(req.query.id);
   const amenityNumber = Number(req.query.amenity_parent);
 
   if (isNaN(number) || typeof number !== "number") {
     res.status(400).send("Invalid request!!");
   }
-  superagent
-    .get(
-      `http://54.177.198.128:8001/api/cat-amenities-childs/?format=json&page=${number}&amenity_parent_id=${amenityNumber}`
-    )
-    .then((response) => {
-      res.status(200).json(JSON.parse(response.text));
-    });
+  const resData = await fetch(
+    `http://54.177.198.128:8001/api/cat-amenities-childs/?format=json&page=${number}&amenity_parent_id=${amenityNumber}`
+  )
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.log(err.message));
+
+  res.status(200).json(resData);
 }
 
 // http://54.177.198.128:8001/api/cat-amenities-childs/?format=json&page=${page}&amenity_parent_id=${amenityParentId}
