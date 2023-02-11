18a36578e366fa862666ad18b668479b286d899e
Angel Rodriguez <angel01rdz@outlook.com>
2023-02-11 01:16:43 -0600

    HTTPS Error fix

--- pages/amenities/[amenityList].js
+++ pages/amenities/[amenityList].js
@@ -31,6 +31,10 @@ export default function AmenityList({ pageSlug }) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{parentData?.data[pageSlug - 1]?.name}</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
       </Head>
       <>
         {childData ? (
